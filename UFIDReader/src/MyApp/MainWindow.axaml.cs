e462c381c8972f58cc189635b38d1ad96a83268e
strejo9811 <strejo@ufl.edu>
2025-02-24 19:23:25 -0500

    Added avalonia with clock and logo

--- UFIDReader/src/MyApp/MainWindow.axaml.cs
+++ UFIDReader/src/MyApp/MainWindow.axaml.cs
@@ -1,11 +1,37 @@
 using Avalonia.Controls;
+using Avalonia.Threading;
+using System;
 
-namespace MyApp;
+namespace MyAvaloniaApp;
 
 public partial class MainWindow : Window
 {
+    private readonly TextBlock _clockText;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        // Find the clock TextBlock
+        _clockText = this.FindControl<TextBlock>("ClockText");
+
+        // Start updating the clock
+        StartClock();
+    }
+
+    private void StartClock()
+    {
+        // Update clock every second
+        DispatcherTimer timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+
+        timer.Tick += (sender, e) =>
+        {
+            _clockText.Text = DateTime.Now.ToString("hh:mm:ss tt"); // Grab time from system
+        };
+
+        timer.Start();
     }
 }
