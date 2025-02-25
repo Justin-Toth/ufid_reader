86a009952f98ccb9b1b5cd172a3bbf399e374865
strejo9811 <strejo@ufl.edu>
2025-02-24 19:46:23 -0500

    Changed gator logo asset

--- UFIDReader/src/MyApp/obj/Debug/net9.0/MyAvaloniaApp.AssemblyInfo.cs
+++ UFIDReader/src/MyApp/obj/Debug/net9.0/MyAvaloniaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAvaloniaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e462c381c8972f58cc189635b38d1ad96a83268e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ecb204e660436d9e72f69de4145f32910e5d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAvaloniaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAvaloniaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
